64d3f0964443adfcebb780aeb5a21a006ac2d829
Julius_Kingsley <hkadam013@protonmail.com>
2026-09-11 11:05:02 +0530

    commited by HVK

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UseEffectHook from "./Components/UseEffectHook";
+import Add from "./Components/Add";
+
+const App = () => {
+  return (
+    <>
+      {/* <UseEffectHook /> */}
+      <Add />
+    </>
+  );
+};
+
+export default App;
